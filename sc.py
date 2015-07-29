7a742171c945e47b1f50247ee484e3ce3d213474
Spencer Gervais <spencergervais21@gmail.com>
2015-07-28 22:49:37 -0400

    Implement creation of directory for artists and download mechanism

--- sc.py
+++ sc.py
@@ -2,12 +2,22 @@
 
 import sys, requests, os, re, json, webbrowser
 
+def strToFile(name):
+    toretr = '_'.join(name.split(' '))
+#Fix annoying common naming convention (ex. artist_name_-_track_name)
+    return re.sub(r'_-_', '-', toretr)
+
+
+#TODO: Add functionality for playlists
+
 
 #TODO: Get URL from command line, confirm it is soundcloud URL
 if len(sys.argv) == 1:
     print "Usage: ./sc.py <Soundcloud URL>"
     sys.exit()
+
 urlregex = re.compile(r'^https://soundcloud.com/.*/.*')
+
 urlmo = urlregex.search(sys.argv[1])
 if urlmo == None:
     print 'Not a valid SoundCloud Track URL'
@@ -31,13 +41,25 @@ artist = jsd['user']['username']
 print trackname
 print artist
 
-filename = '_'.join(trackname.split(' '))
-dirname = '_'.join(trackname.split(' '))
+filename = strToFile(trackname)
+dirname = strToFile(artist)
 
-#Annoying Look for filename with single dash
-weirdDash = re.compile(r'_-_')
-
-
-
+print dirname
+print filename
 #TODO: Create file and download track to that file
 
+if dirname not in os.listdir(os.getcwd()):
+    os.makedirs(os.path.join(os.getcwd(), dirname))
+try:
+    streamres = requests.get(streamurl)
+    streamres.raise_for_status()
+except:
+    print "Could not connect to stream"
+    sys.exit()
+
+f = open(os.path.join(os.getcwd(), dirname, filename) + '.mp3', 'wb')
+
+for chunk in streamres.iter_content(100000):
+    f.write(chunk)
+
+f.close()
