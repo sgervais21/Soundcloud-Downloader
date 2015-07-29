6ba66210c9d9d8e7a7785f5df7191557e6d161b9
Spencer Gervais <spencergervais21@gmail.com>
2015-07-28 23:34:08 -0400

    Add file size

--- sc.py
+++ sc.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python
 
-import sys, requests, os, re, json, webbrowser
+import sys, requests, os, re, json, webbrowser, time
 
 def strToFile(name):
     toretr = '_'.join(name.split(' '))
-#Fix annoying common naming convention (ex. artist_name_-_track_name)
+    #Fix annoying common naming convention (ex. artist_name_-_track_name)
     return re.sub(r'_-_', '-', toretr)
+
+def fileSize(bytes):
+    if bytes < 1000:
+        return str(bytes) + " bytes"
+    elif bytes < 1000 ** 2:
+        return "{:.1f} kB".format(bytes/1000.0)
+    elif bytes < 1000 ** 3:
+        return "{:.1f} MB".format(bytes/1000000.0)
 
 
 #TODO: Add functionality for playlists
 
 
 #TODO: Get URL from command line, confirm it is soundcloud URL
+
 if len(sys.argv) == 1:
     print "Usage: ./sc.py <Soundcloud URL>"
     sys.exit()
@@ -22,15 +31,17 @@ urlmo = urlregex.search(sys.argv[1])
 if urlmo == None:
     print 'Not a valid SoundCloud Track URL'
     sys.exit()
-url = 'http://api.soundcloud.com/resolve?url=' + urlmo.group(0) + '&client_id=YOUR_CLIENT_ID'
+url = 'http://api.soundcloud.com/resolve?url=' + urlmo.group(0) +                      '&client_id=YOUR_CLIENT_ID'
 
 #TODO: Get access to JSON from URL
+
 try: 
     res = requests.get(url)
     res.raise_for_status()
 except:
     print "Not a valid URL"
     sys.exit()
+
 jsd = json.loads(res.text)
 streamurl = 'https://api.soundcloud.com/tracks/' + str(jsd['id']) + '/stream?client_id=YOUR_CLIENT_ID'
 
@@ -38,14 +49,12 @@ streamurl = 'https://api.soundcloud.com/tracks/' + str(jsd['id']) + '/stream?cli
 
 trackname = jsd['title']
 artist = jsd['user']['username']
-print trackname
-print artist
+approx_bytes = (jsd['duration'] / 1000) * 16000 #approximation at 128 kbps
 
 filename = strToFile(trackname)
 dirname = strToFile(artist)
+size = fileSize(approx_bytes)
 
-print dirname
-print filename
 #TODO: Create file and download track to that file
 
 if dirname not in os.listdir(os.getcwd()):
@@ -57,9 +66,13 @@ except:
     print "Could not connect to stream"
     sys.exit()
 
-f = open(os.path.join(os.getcwd(), dirname, filename) + '.mp3', 'wb')
-
-for chunk in streamres.iter_content(100000):
-    f.write(chunk)
-
-f.close()
+if (filename + '.mp3') not in os.listdir(os.path.join(os.getcwd(), dirname)):
+    f = open(os.path.join(os.getcwd(), dirname, filename) + '.mp3', 'wb')
+    print '\n' + "Now Downloading"
+    print "Track: " + trackname
+    print "Artist: " + artist
+    print "Size: " + size
+    count = 0.0
+    for chunk in streamres.iter_content(1000):
+        f.write(chunk)
+    f.close()
